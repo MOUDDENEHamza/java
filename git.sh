bdb6c56100308eaeccad6cffb227977835d31e36
hamza <hz.mouddene@gmail.com>
2019-03-19 21:48:53 +0100

    9292message

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ $# -ne 0 ] ; then
+	echo "ERROR : Wrong usage\nUsage : $0" >&2
+		exit 1
+fi
+
+git add .
+@read "Enter the message to set up the commit : " message;
+git commit -m "$$message"
+git push
